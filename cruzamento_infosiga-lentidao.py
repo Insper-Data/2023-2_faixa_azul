64e3de9a6ef48c62ac7b95e5c6f8da1d19285cda
gustavo-tm <gtheilm@gmail.com>
2023-11-10 18:11:47 -0300

    tratamento base lentidao, fixes

--- cruzamento_infosiga-lentidao.py
+++ cruzamento_infosiga-lentidao.py
@@ -1,58 +1,65 @@
 import pandas as pd
 import numpy as np
+import pathlib
 import json 
+import os
+from download_infosiga import download_infosiga
 
-avenidas = (pd.read_csv("dados/acidentes_naofatais.csv", encoding = "latin", sep = ";")
-.rename({
-    "Data do Acidente": "data",
-    "Logradouro": "rua",
-    "Município": "nm_municipio",
-    "Veículos Envolvidos - Motocicleta": "motocicletas",
-    "Pessoas Envolvidas - Leve": "leve",
-    "Pessoas Envolvidas - Grave": "grave",
-}, axis = 1)
-.filter(["data", "nm_municipio", "rua", "grave", "leve", "motocicletas"])
-.query(f"(grave > 0 or leve > 0) and motocicletas > 0 and nm_municipio == 'SAO PAULO'")
-.assign(
-    mes = lambda _: _.data.str[5:7], 
-    ano = lambda _: _.data.str[0:4])
-.replace({"rua": {"'": " "}}, regex = True)
-.groupby(["ano", "rua"])
-.aggregate({"motocicletas": "count"})
-.reset_index()
-.sort_values(by = ["ano", "motocicletas"])
-.query("ano == '2021' and ~ rua.str.contains('SP ') and ~ rua.str.contains('SPA ') and ~ rua.str.contains('BR ')")
-.tail(80)
-)
+if not (pathlib.Path.cwd() / "cruzamento_infosiga-lentidao.json").exists():
+    avenidas = (pd.read_csv("dados/acidentes_naofatais.csv", encoding = "latin", sep = ";")
+    .rename({
+        "Data do Acidente": "data",
+        "Logradouro": "rua",
+        "Município": "nm_municipio",
+        "Veículos Envolvidos - Motocicleta": "motocicletas",
+        "Pessoas Envolvidas - Leve": "leve",
+        "Pessoas Envolvidas - Grave": "grave",
+    }, axis = 1)
+    .filter(["data", "nm_municipio", "rua", "grave", "leve", "motocicletas"])
+    .query(f"(grave > 0 or leve > 0) and motocicletas > 0 and nm_municipio == 'SAO PAULO'")
+    .assign(
+        mes = lambda _: _.data.str[5:7], 
+        ano = lambda _: _.data.str[0:4])
+    .replace({"rua": {"'": " "}}, regex = True)
+    .groupby(["ano", "rua"])
+    .aggregate({"motocicletas": "count"})
+    .reset_index()
+    .sort_values(by = ["ano", "motocicletas"])
+    .query("ano == '2021' and ~ rua.str.contains('SP ') and ~ rua.str.contains('SPA ') and ~ rua.str.contains('BR ')")
+    .tail(80)
+    )
 
-lentidao = (
-    pd.read_csv("dados/lentidao.csv", encoding="latin-1", sep = ",")
-    pd.read_csv("dados/lentidao_2022.csv", encoding="latin", sep = ";")
-    .replace({"corredor": {"Av": "Avenida", ",": "", "/": " ", "\(": " ", "\)": " ", "\.": " "}}, regex = True)
-)
+    lentidao = (
+        pd.read_csv("dados/lentidao.csv", encoding="latin-1", sep = ",")
+        .replace({"corredor": {"Av": "Avenida", ",": "", "/": " ", "\(": " ", "\)": " ", "\.": " "}}, regex = True)
+    )
 
-banlist = ["AVENIDA", "ESTRADA", "DOS", "DE", "DO", "DA", "RUA"]
+    banlist = ["AVENIDA", "ESTRADA", "DOS", "DE", "DO", "DA", "RUA"]
 
-dicio_regex = {
-    avenida: "|".join([palavra for palavra in avenida.split(" ") if palavra not in banlist and len(palavra) > 1]) 
-    for avenida in avenidas.rua.to_list()
-}
+    dicio_regex = {
+        avenida: "|".join([palavra for palavra in avenida.split(" ") if palavra not in banlist and len(palavra) > 1]) 
+        for avenida in avenidas.rua.to_list()
+    }
 
-dicio_conversao = {}
-for avenida, regex in dicio_regex.items():
-    valores = lentidao.query(f"corredor.str.upper().str.contains('{(regex)}')").corredor.unique()
-    if len(valores) == 0:
-        dicio_conversao.update({avenida: None})
-    else:
-        print(avenida, "->", regex)
-        print(dict({0: "Sair", 1: "Nenhuma das opções"}, **{str(numero + 2): valores[numero] for numero in range(len(valores))}))
-        try: resposta = int(input())
-        except:
-            print("ERRO")
-            resposta = int(input())
-        if resposta == 0: break
-        elif resposta == 1: dicio_conversao.update({avenida: None})
-        else: dicio_conversao.update({avenida: valores[resposta - 2]})
+    dicio_conversao = {}
+    for avenida, regex in dicio_regex.items():
+        valores = lentidao.query(f"corredor.str.upper().str.contains('{(regex)}')").corredor.unique()
+        if len(valores) == 0:
+            dicio_conversao.update({avenida: None})
+        else:
+            print(avenida, "->", regex)
+            print(dict({0: "Sair", 1: "Nenhuma das opções"}, **{str(numero + 2): valores[numero] for numero in range(len(valores))}))
+            try: resposta = int(input())
+            except:
+                print("ERRO")
+                resposta = int(input())
+            if resposta == 0: break
+            elif resposta == 1: dicio_conversao.update({avenida: None})
+            else: dicio_conversao.update({avenida: valores[resposta - 2]})
 
-with open("cruzamento_infosiga-lentidao.json", "w") as outfile:
-    outfile.write(json.dumps(dicio_conversao))
+
+    with open("cruzamento_infosiga-lentidao.json", "w") as outfile:
+        outfile.write(json.dumps(dicio_conversao))
+
+if not (pathlib.Path.cwd() / "dados" / "acidentes_naofatais.csv").exists():
+    download_infosiga()
--- download_infosiga.py
+++ download_infosiga.py
@@ -3,26 +3,29 @@ import pathlib
 import os
 import pandas as pd
 
-## dicionario com os nomes dos arquivos e suas respectivas urls
-dados = {
-    'acidentes_naofatais.csv' : 'http://painelderesultados.infosiga.sp.gov.br/bases/acidentes_naofatais.csv',
-    'obitos_publico.csv' : 'http://painelderesultados.infosiga.sp.gov.br/bases/obitos_publico.csv' 
-}
+def download_infosiga():
+    ## dicionario com os nomes dos arquivos e suas respectivas urls
+    dados = {
+        'acidentes_naofatais.csv' : 'http://painelderesultados.infosiga.sp.gov.br/bases/acidentes_naofatais.csv',
+        'obitos_publico.csv' : 'http://painelderesultados.infosiga.sp.gov.br/bases/obitos_publico.csv' 
+    }
 
-DATA_DIR = pathlib.Path.cwd() / 'dados'
-DATA_DIR.mkdir(exist_ok=True, parents=True)
+    DATA_DIR = pathlib.Path.cwd() / 'dados'
+    DATA_DIR.mkdir(exist_ok=True, parents=True)
 
-for nome_arquivo, url in dados.items():
-    path_dados = DATA_DIR / nome_arquivo
-    if not path_dados.exists():
-        print(f'Fazendo download de {nome_arquivo}')
-        headers = {
-        'User-Agent': \
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' \
-            'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-            'Chrome/39.0.2171.95 Safari/537.36',
-        }
-        response = requests.get(url, headers=headers)
-        csv_content = response.content.decode(encoding='latin-1')
-        with open(path_dados, 'w', encoding='latin-1') as file:
-            file.write(csv_content)
+    for nome_arquivo, url in dados.items():
+        path_dados = DATA_DIR / nome_arquivo
+        if not path_dados.exists():
+            print(f'Fazendo download de {nome_arquivo}')
+            headers = {
+            'User-Agent': \
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' \
+                'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                'Chrome/39.0.2171.95 Safari/537.36',
+            }
+            response = requests.get(url, headers=headers)
+            csv_content = response.content.decode(encoding='latin-1')
+            with open(path_dados, 'w', encoding='latin-1') as file:
+                file.write(csv_content)
+
+download_infosiga()
